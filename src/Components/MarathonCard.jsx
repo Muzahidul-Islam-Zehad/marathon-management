523bc37548332d4a2debdf4f31f6229b002cab5a
Muzahidul-Islam-Zehad <zehadkazi88@gmail.com>
2025-02-12 16:12:37 +0600

    marathon cards designed for dark mode

--- src/Components/MarathonCard.jsx
+++ src/Components/MarathonCard.jsx
@@ -2,18 +2,21 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
+import { useContext } from "react";
+import { contextProvider } from "../Providers/AuthProvider";
 const MarathonCard = ({ marathon }) => {
+    const {isDark} = useContext(contextProvider);
 
     return (
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col">
+        <div className={`${isDark ? 'bg-[#444242]' : 'bg-white'}  shadow-lg rounded-lg overflow-hidden flex flex-col`}>
             {/* Image */}
             <img src={marathon.image} alt={marathon.title} className="h-48 w-full object-cover" />
 
             {/* Content */}
             <div className="p-4 flex-1">
-                <h3 className="text-lg font-bold mb-2">{marathon.title}</h3>
-                <p className="text-sm mb-4">Location: {marathon.location}</p>
-                <p className="text-sm mb-4">
+                <h3 className={`text-lg font-bold mb-2 ${isDark && 'text-[#d69327]'}`}>{marathon.title}</h3>
+                <p className={`text-sm mb-4 ${isDark && 'text-white'}`}>Location: {marathon.location}</p>
+                <p className={`text-sm mb-4 ${isDark && 'text-white'}`}>
                     Registration: {format(marathon?.startRegistration , "P" ,{locale: fr })} - {format(marathon?.endRegistration , "P" , {locale: fr})}
                 </p>
             </div>
@@ -22,7 +25,7 @@ const MarathonCard = ({ marathon }) => {
             <div className="mt-auto p-4">
                 <Link to={`/marathon-details/${marathon._id}`}>
                     <button
-                        className="btn btn-primary w-full grow"
+                        className={`btn  w-full grow ${isDark ? `btn-outline text-[#d69327] bg-[#1e1e1e] hover:bg-[#d69327] hover:text-white`: `btn-primary`}`}
                     >
                         See Details
                     </button>
